b027389349dcc463e0b45b20eb06df81d2a07bf2
David Carne <davidcarne@gmail.com>
2009-11-01 00:29:22 -0400

    Line endings, Licensing

--- src/net_group.h
+++ src/net_group.h
@@ -1,3 +1,26 @@
+/*
+ *  Portions Copyright 2006,2009 David Carne and 2007,2008 Spark Fun Electronics
+ *
+ *
+ *  This file is part of gerberDRC.
+ *
+ *  gerberDRC is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  gerberDRC is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+#ifndef _NET_GROUP_H_
+#define _NET_GROUP_H_
 
 /********************* net_group *********************/
 net_group * new_net_group(Vector_Outp * f);
@@ -24,3 +47,6 @@ private:
 	int set_id;
 	std::set <GerbObj*> members;
 };
+
+#endif
+
